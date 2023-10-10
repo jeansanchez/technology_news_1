be927b587aeadf5c4232f5abd38c409ff8bdf620
emiro <emiro1492@example.com>
2023-10-09 21:27:21 -0500

    Implemented CRUD Operations for Admins and Editors, Database Connection

---
+++ server/src/controllers/users.controllers.js
@@ -0,0 +1,62 @@
+import { pool } from "../db.js";
+
+export const getUsers = async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM admins");
+  res.json(rows);
+};
+
+export const getUser = async (req, res) => {
+  const [rows] = await pool.query("SELECT * FROM admins WHERE id = ?", [
+    req.params.id,
+  ]);
+  if (rows.length <= 0)
+    return res.status(404).json({
+      message: "User not found",
+    });
+  res.json(rows[0]);
+};
+
+export const createUsers = async (req, res) => {
+  const { name, lastname, username, email, password, role } = req.body;
+  const [rows] = await pool.query(
+    "INSERT INTO admins(name, lastname, username, email, password, role) VALUES (?, ?, ?, ?, ?, ?)",
+    [name, lastname, username, email, password, role]
+  );
+  res.send({
+    id: rows.insertId,
+    name,
+    lastname,
+    username,
+    email,
+    password,
+    role,
+  });
+};
+
+export const deleteUsers = async (req, res) => {
+  const [result] = await pool.query("DELETE FROM admins WHERE id = ?", [
+    req.params.id,
+  ]);
+  if (result.affectedRows <= 0)
+    return res.status(404).json({
+      message: "User not found",
+    });
+  res.sendStatus(204);
+};
+
+export const updateUsers = async (req, res) => {
+  const { id } = req.params;
+  const { name, lastname, username, email, password, role } = req.body;
+
+  const [result] = await pool.query(
+    "UPDATE admins SET name = IFNULL(?, name), lastname = IFNULL(?, lastname), username = IFNULL(?, username), email = IFNULL(?, email), password = IFNULL(?, password), role = IFNULL(?, role) WHERE id = ?",    [name, lastname, username, email, password, role, id]
+  );
+  if (result.affectedRows === 0)
+    return res.status(404).json({
+      message: "User not found",
+    });
+
+  const [rows] = await pool.query("SELECT * FROM admins WHERE id = ?", [id]);
+
+  res.json(rows[0]);
+};
---
+++ server/src/routes/users.routes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { getUsers, createUsers, updateUsers, deleteUsers, getUser } from "../controllers/users.controllers.js"
+
+const router = Router() 
+
+router.get('/users/', getUsers)
+router.get('/users/:id/', getUser)
+router.post('/users', createUsers)
+router.patch('/users/:id/', updateUsers)
+router.delete('/users/:id/', deleteUsers)
+
+export default router;
