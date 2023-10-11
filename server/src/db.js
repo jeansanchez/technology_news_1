3db81936a0e00664e27959fd2415352a576108e2
emiro <emiro1492@example.com>
2023-10-10 20:59:37 -0500

    Error handling

--- server/src/db.js
+++ server/src/db.js
@@ -1,9 +1,9 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Ajedrez14-17',
-    port: 3306, 
-    database: 'users'
+  host: "localhost",
+  user: "root",
+  password: "Ajedrez14-17",
+  port: 3306,
+  database: "users",
 });
