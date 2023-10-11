230528a4a47d7e544a79ae718fcd7e352d238f3c
emiro <emiro1492@example.com>
2023-10-10 20:59:37 -0500

    Error handling

--- server/database/database.sql
+++ server/database/database.sql
@@ -13,9 +13,3 @@ CREATE TABLE admins (
     PRIMARY KEY (id)
 );
 
-DESCRIBE admins; 
-
-INSERT INTO admins VALUES 
-(1, 'joe', 'hands', 'Jhands', 'jh@gmail.com', '12jk', 'admin'),
-(2, 'matt', 'gush', 'Mgush', 'mg@gmail.com', '1eqfbi2jk', 'admin'), 
-(3, 'harry', 'fan', 'Hfan', 'hf@gmail.com', '1ergqli2', 'editor');
--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,17 @@
 import express from "express";
-import { pool } from "./db.js";
 import usersRoutes from "./routes/users.routes.js";
 import morgan from "morgan";
 
 const app = express();
-app.use(express.json())
-app.use(morgan('dev'));
+app.use(express.json());
+app.use(morgan("dev"));
 
-app.use('/api/', usersRoutes)
+app.use("/api/", usersRoutes);
 
-app.listen(3000)
+app.use((req, res, next) => {
+  res.status(404).json({message: 'Not found'})
+})
+
+app.listen(3000);
 
 
-
-app.get('/ping/', async (req, res) => {
-    const [result] =  await pool.query('SELECT 1 + 1 AS result');
-    res.json(result);
-  })
