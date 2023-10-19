65c10856712f336a0f4109d6ee0482330779f3ea
emiro <emiro1492@example.com>
2023-10-18 22:01:03 -0500

    Folder organization

--- server/src/db.js
+++ server/src/db.js
@@ -1,9 +1,13 @@
 import { createPool } from "mysql2/promise";
+import { config } from "dotenv";
+
+config();
 
 export const pool = createPool({
-  host: "localhost",
-  user: "root",
-  password: "Ajedrez14-17",
-  port: 3306,
-  database: "users",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 });
+
+
--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import usersRoutes from "./routes/users.routes.js";
 import morgan from "morgan";
 
+ 
+
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
